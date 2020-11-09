396c48ba138e5b28937661b392f30473fbdaf87f
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2020-11-09 10:04:56 -0600

    Add files via upload

--- main.go
+++ main.go
@@ -2,14 +2,68 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"image/png"
+	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 
+	"github.com/go-vgo/robotgo"
 	"github.com/gorilla/mux"
 	"github.com/kbinani/screenshot"
 )
 
+type com struct {
+	com string
+}
+
+func clearRequest(str string) string {
+	str = strings.Replace(str, "{", "", -1)
+	str = strings.Replace(str, "}", "", -1)
+	str = strings.Replace(str, "\"", "", -1)
+	return str
+
+}
+func madeMapMouse(str string) map[string]int {
+	str = clearRequest(str)
+	strV := strings.Split(str, ",")
+	m := map[string]int{}
+	for _, i := range strV {
+		n := strings.Split(i, ":")
+
+		l, _ := strconv.Atoi(n[1])
+
+		m[n[0]] = l
+	}
+	return m
+}
+
+func bodyRequest(r *http.Request) string {
+	body, _ := ioutil.ReadAll(r.Body)
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	//first decode the request body
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	newStr := buf.String()
+
+	return newStr
+
+}
+func readMouse(w http.ResponseWriter, r *http.Request) {
+	newStr := bodyRequest(r)
+	m := madeMapMouse(newStr)
+	robotgo.MoveMouse(m["x"], m["y"])
+}
+func readCommand(w http.ResponseWriter, r *http.Request) {
+	var c com
+	req := bodyRequest(r)
+	j := json.NewDecoder(strings.NewReader(req))
+	j.Decode(&c)
+	fmt.Println(c, req)
+
+}
 func sendI(w http.ResponseWriter, r *http.Request) {
 	n := screenshot.NumActiveDisplays()
 	for i := 0; i < n; i++ {
@@ -17,14 +71,20 @@ func sendI(w http.ResponseWriter, r *http.Request) {
 		img, _ := screenshot.CaptureRect(bounds)
 		buffer := new(bytes.Buffer)
 		png.Encode(buffer, img)
+		//image
 		w.Header().Set("Content-Type", "image/jpeg")
 		w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+		//image
 		w.Write(buffer.Bytes())
 	}
 }
 func main() {
 	r := mux.NewRouter()
+
+	fs := http.Dir("page/")
+	r.Handle("/", http.StripPrefix("/", http.FileServer(fs)))
 	r.HandleFunc("/image/{a}", sendI)
-	r.Handle("/", http.FileServer(http.Dir("./page")))
+	r.HandleFunc("/mouse", readMouse)
+	r.HandleFunc("/command", readCommand)
 	http.ListenAndServe(":8090", r)
 }
