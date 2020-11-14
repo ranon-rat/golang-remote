a9d9fbf43d8500652b4983493897ad650ac1bd9d
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2020-11-14 02:53:37 -0600

    new updates
    now doesnt have to decode to a map , now decode to a json, you can made click  and type something

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
+	"image"
 	"image/png"
 	"io/ioutil"
 	"net/http"
@@ -15,79 +16,86 @@ import (
 	"github.com/kbinani/screenshot"
 )
 
-type com struct {
-	com string
+type mouse struct {
+	X     int             `json:"x"`
+	Y     int             `json:"y"`
+	Click clickAtrributes `json:"clickAttr"`
+}
+type clickAtrributes struct {
+	Click bool   `json:"click"`
+	Side  string `json:"side"`
+}
+type command struct {
+	Command string `json:"command"`
+}
+type text struct {
+	Text string `json:"word"`
 }
 
-func clearRequest(str string) string {
-	str = strings.Replace(str, "{", "", -1)
-	str = strings.Replace(str, "}", "", -1)
-	str = strings.Replace(str, "\"", "", -1)
-	return str
-
-}
-func madeMapMouse(str string) map[string]int {
-	str = clearRequest(str)
-	strV := strings.Split(str, ",")
-	m := map[string]int{}
-	//idk why doesnt made the json for that i do this
-	for _, i := range strV {
-		n := strings.Split(i, ":")
-
-		l, _ := strconv.Atoi(n[1])
-
-		m[n[0]] = l
+//---------------------> this was the tools for decode or compress
+func compress(img image.Image) image.Image {
+	var division float64 = 1.2
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{int(float64(img.Bounds().Max.X) / division), int(float64(img.Bounds().Max.Y) / division)}
+	img2 := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	for x := 0; x < int(float64(img.Bounds().Max.X)/division); x++ {
+		for y := 0; y < int(float64(img.Bounds().Max.Y)/division); y++ {
+			img2.Set(x, y, img.At(int(float64(x)*division), int(float64(y)*division)))
+		}
 	}
-	return m
+	return img2
 }
 
 func bodyRequest(r *http.Request) string {
 	body, _ := ioutil.ReadAll(r.Body)
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-	//first decode the request body
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(r.Body)
-	newStr := buf.String()
-	//decode the request to a string
-	return newStr
 
+	newStr := buf.String()
+	return newStr
 }
+
+//--------------------->  here is the remote control functions
 func readMouse(w http.ResponseWriter, r *http.Request) {
+	var mo mouse
 	newStr := bodyRequest(r)
-	m := madeMapMouse(newStr)
-	robotgo.MoveMouse(m["x"], m["y"])
-	//is only for move the mouse, then i gona make something interesting
+	json.Unmarshal([]byte(newStr), &mo)
+	robotgo.MoveMouse(mo.X, mo.Y)
+	if mo.Click.Click {
+		robotgo.MouseClick(mo.Click.Side, mo.Click.Click)
+	}
+
 }
 func readCommand(w http.ResponseWriter, r *http.Request) {
-
-	req := bodyRequest(r)
-
-	req = clearRequest(req)
-	req1 := strings.Split(req, ":")
-	command := strings.Split(req1[1], " ")
-	cmd := exec.Command(command[0], command[1:]...)
-	fmt.Println(command)
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-	}
-	out, _ := cmd.Output()
-	fmt.Println(string(out))
-	fmt.Println()
-
-	j := json.NewDecoder(strings.NewReader(req))
-	j.Decode(&c)//idk why doest decode the request 
-	fmt.Println(c, req)
-
+	mo := new(command)
+	newStr := bodyRequest(r)
+	json.Unmarshal([]byte(newStr), &mo)
+	comm := strings.Split(mo.Command, " ")
+	cmd := exec.Command(comm[0], comm[1:]...)
+	cmd.Run()
 
 }
+func typeSomething(w http.ResponseWriter, r *http.Request) {
+	var mo text
+	newStr := bodyRequest(r)
+	json.Unmarshal([]byte(newStr), &mo)
+	robotgo.TypeStr(mo.Text)
+}
+
+//--------------------->  this is for get the image of the screen and send it
 func sendI(w http.ResponseWriter, r *http.Request) {
+
 	n := screenshot.NumActiveDisplays()
+
 	//this only take screenshots and send to the page
 	for i := 0; i < n; i++ {
+
 		bounds := screenshot.GetDisplayBounds(i)
+
 		img, _ := screenshot.CaptureRect(bounds)
 		buffer := new(bytes.Buffer)
-		png.Encode(buffer, img)
+		png.Encode(buffer, compress(img))
 		//image encode the image and send the image
 		w.Header().Set("Content-Type", "image/jpeg")
 		w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
@@ -95,13 +103,15 @@ func sendI(w http.ResponseWriter, r *http.Request) {
 		w.Write(buffer.Bytes())
 	}
 }
+
+//--------------------->  the main
 func main() {
 	r := mux.NewRouter()
-
 	fs := http.Dir("page/")
-	r.Handle("/", http.StripPrefix("/", http.FileServer(fs)))
+	r.Handle("/", http.FileServer(fs))
 	r.HandleFunc("/image/{a}", sendI)
 	r.HandleFunc("/mouse", readMouse)
+	r.HandleFunc("/typetext", typeSomething)
 	r.HandleFunc("/command", readCommand)
 	http.ListenAndServe(":8090", r)
 }
